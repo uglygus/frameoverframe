3facb3ad55a38f18574513ff5897ecc4103bb218
uglygus <cooperbattersby@gmail.com>
2021-06-14 14:27:02 -0400

    make_previews seems to work

--- frameoverframe/cli/make_previews__main__.py
+++ frameoverframe/cli/make_previews__main__.py
@@ -4,15 +4,17 @@ make_previews.py
 
 given a folder check every subfolder and try to run img2vid on it.
 
-
 """
 
 import argparse
+import logging.config
 import os
+import re
 import sys
 
 from colorama import Fore, Style, init
 
+from frameoverframe.config import LOGGING_CONFIG, RAW_EXTENSIONS
 from frameoverframe.img2vid import img2vid
 from frameoverframe.utils import ext_list, sorted_listdir
 
@@ -27,6 +29,10 @@ def collect_args():
         help="Source directory. ",
     )
 
+    parser.add_argument("--quiet", action="store_true", help="Only output when necessary.")
+    parser.add_argument("--verbose", "-v", action="store_true", help="increase output verbosity")
+    parser.add_argument("--debug", action="store_true", help="Print debugging information.")
+
     return parser
 
 
@@ -37,39 +43,64 @@ def main():
 
     """
 
-    init()  # colorama
-
     parser = collect_args()
     args = parser.parse_args()
 
+    logging.config.dictConfig(LOGGING_CONFIG)
+    log = logging.getLogger("frameoverframe")
+
+    log.setLevel(logging.WARN) if args.quiet else None
+    log.setLevel(logging.INFO) if args.verbose else None
+    log.setLevel(logging.DEBUG) if args.debug else None
+
+    init()  # colorama
+
+    from itertools import product
+
+    # input("...stopee....")
+
     for _dir in sorted_listdir(args.src_dir):
+        # inpu("...waiting..top of for....")
+        skip_this_dir = False
+        log.info(f"{_dir}")
 
-        # print("\n" + f"{_dir=}")
+        if os.path.isfile(_dir):
+            continue
 
-        if os.path.isdir(_dir):
-            if os.path.isfile(_dir + ".mp4"):
-                pass
-                # print(f"Video file for {args.src_dir}/{_dir} already exists.")
+        if os.path.isfile(f"{_dir}.mp4") or os.path.isfile(f"{_dir}_preview.mp4"):
+            pass
+            log.info(f"Video file for {args.src_dir}/{_dir} already exists.")
+            continue
+
+        extensions = ext_list(_dir)
+        log.debug(f"{extensions=}")
+        #    print(f"'{_dir}' -- Needs video.")
+
+        if extensions == [""]:
+            log.info(
+                f"'{_dir}' -- {Fore.RED}SKIPPING{Style.RESET_ALL} Folder contains only directories."
+            )
+            continue
+
+        #        for ext in extensions:
+
+        for ext, raw_ext in product(extensions, RAW_EXTENSIONS):
+            # print(f"{ext=}, {raw_ext=}")
+            if re.search(rf"{ext}$", raw_ext, re.IGNORECASE):
+                log.debug(" MATCH contains ext", ext)
+                skip_this_dir = True
+                break
             else:
-                extensions = ext_list(_dir)
-                print(f"'{_dir}' -- Needs video.")
+                log.debug("NO-MATCH")
 
-                if extensions == [""]:
-                    print(
-                        f"'{_dir}' -- {Fore.RED}SKIPPING{Style.RESET_ALL} Folder contains only directories."
-                    )
-                    continue
-                if ".ARW" in extensions:
-                    print(
-                        f"'{_dir}' -- {Fore.RED}SKIPPING{Style.RESET_ALL} Folder contains .ARW files. I only process .JPG files."
-                    )
-                    continue
-                if ".CR2" in extensions:
-                    print(
-                        f"'{_dir}' -- {Fore.RED}SKIPPING{Style.RESET_ALL} Folder contains .CR2 files. I only process .JPG files."
-                    )
-                    continue
-                img2vid(_dir, _dir + ".mp4")
+        # print("....done for loop...")
+        # input("...")
+
+        if skip_this_dir:
+            continue
+
+        log.info(f"calling image2vid({_dir}, {_dir}.mp4)")
+        img2vid(_dir, _dir + ".mp4")
 
 
 if __name__ == "__main__":
