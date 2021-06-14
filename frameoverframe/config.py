c5e5cfb49d653042163f7555fca6a406381c525d
uglygus <cooperbattersby@gmail.com>
2021-06-14 14:26:40 -0400

    update CONFIG

--- frameoverframe/config.py
+++ frameoverframe/config.py
@@ -1,3 +1,46 @@
 #!/usr/bin/env python3
 
-extensions = ["JPG", "ARW", "CR2"]
+# extensions = ["JPG", "ARW", "CR2"]  # not sure if this ever gets used??
+
+# These should be case insensitive. Hopefully.
+RAW_EXTENSIONS = [".CR2", ".ARW", ".NEF", ".DNG"]
+
+
+logfile = "fof.log"
+
+LOGGING_CONFIG = {
+    "version": 1,
+    "formatters": {
+        "default": {
+            "format": "%(asctime)s -  %(levelname)s - %(message)s",
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        }
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "default",
+            "stream": "ext://sys.stdout",
+        },
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.handlers.RotatingFileHandler",
+            "formatter": "default",
+            "filename": logfile,
+            "maxBytes": 10485760,  # 10MB
+            "backupCount": 3,
+        },
+    },
+    # include "file" in the list of handlers to log to the file too.
+    "loggers": {
+        #    "": {"level": "WARN", "handlers": ["console"]},  # root Logger
+        "frameoverframe": {
+            "level": "WARN",
+            "handlers": [
+                "console",
+            ],
+        },
+    },
+    "disable_existing_loggers": False,
+}
