fe26df8514ebfacfd91d4f099028d4109fda06d2
uglygus <cooperbattersby@gmail.com>
2021-06-16 22:38:10 -0400

    vid2img- logging

--- frameoverframe/cli/vid2img__main__.py
+++ frameoverframe/cli/vid2img__main__.py
@@ -9,8 +9,8 @@
 import argparse
 import logging.config
 
-#   logging.getLogger() has to come before importing any frameoverframe modules
-#   except frameoverframe.config that must come before (careful isort might move imports)
+#   logging.getLogger() must come after frameoverframe.config
+#    but before any other frameoverframe modules.
 from frameoverframe.config import LOGGING_CONFIG
 
 logging.config.dictConfig(LOGGING_CONFIG)
@@ -28,6 +28,7 @@ def collect_args():
     )
 
     parser.add_argument("input_mov", help="video file...")
+    group = parser.add_mutually_exclusive_group()
     parser.add_argument(
         "-o",
         "--output_folder",
@@ -35,8 +36,26 @@ def collect_args():
         default=None,
         help="output folder, default is the name of name_of_input_video",
     )
+    group.add_argument(
+        "--quiet",
+        "-q",
+        action="store_const",
+        const=logging.WARN,
+        dest="loglevel",
+        help="Only output when necessary.",
+    )
+    group.add_argument(
+        "--verbose",
+        "-v",
+        action="store_const",
+        const=logging.DEBUG,
+        dest="loglevel",
+        help="Increase output verbosity.",
+    )
 
+    parser.set_defaults(loglevel=logging.INFO)
     args = parser.parse_args()
+    log.setLevel(args.loglevel)
 
     return args
 
@@ -45,14 +64,20 @@ def main():
     """commandline setup vid2img"""
 
     args = collect_args()
-    log.setLevel(logging.INFO)
+    log.setLevel(args.loglevel)
 
     log.debug(f"args= {args}")
+    log.debug(f"{log.level=}")
 
-    vid2img(args.input_mov, args.output_folder)
+    try:
+        vid2img(args.input_mov, args.output_folder)
+    except FileNotFoundError as e:
+        print("FileNotFoundError: ", e)
+        return 1
 
-    print("DONE.")
+    log.info("DONE.")
+    return 0
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
--- frameoverframe/vid2img.py
+++ frameoverframe/vid2img.py
@@ -32,18 +32,26 @@ def vid2img(input_mov, output_folder=None):
 
     ffmpeg_bin = shutil.which("ffmpeg")
 
-    if ffmpeg_bin:
-        sys_call = [
-            ffmpeg_bin,
+    if ffmpeg_bin == None:
+        raise FileNotFoundError("FFMPEG binary not found in your $PATH.")
+
+    if log.level <= logging.DEBUG:
+        ff_logflags = []
+    elif log.level == logging.INFO:
+        ff_logflags = ["-loglevel", "error", "-stats"]
+    elif log.level >= logging.WARN:
+        ff_logflags = ["-loglevel", "error", "-nostats"]
+
+    sys_call = [ffmpeg_bin]
+    sys_call.extend(ff_logflags)
+    sys_call.extend(
+        [
             "-i",
             input_mov,
             output_folder + "/" + filename + "_" + "%08d" + ".png",
         ]
+    )
 
-        calling_log = "\ncalling : ", " ".join(quote(sys_call)), "\n"
-        log.info(calling_log)
-        subprocess.call(sys_call)
-
-    else:
-        log.warn("ERROR: ffmpeg is required and is not intstalled. ")
-        raise FileNotFoundError("ffmpeg")
+    calling_log = "Calling : " + " ".join(quote(sys_call))
+    log.info(calling_log)
+    subprocess.call(sys_call)
