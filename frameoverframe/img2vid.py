5315dad92a15a6e6024c44fcede9845cd3e42946
uglygus <cooperbattersby@gmail.com>
2021-06-20 21:53:56 -0400

    Logging utils (#48)
    * logging - utils
    
    * logging - test_images
    
    * vid2img - verbosity instead of group in collect_args()

--- frameoverframe/img2vid.py
+++ frameoverframe/img2vid.py
@@ -34,7 +34,9 @@ from PIL import Image
 from frameoverframe.config import RAW_EXTENSIONS
 from frameoverframe.utils import sorted_listdir, test_one_extension
 
+
 log = logging.getLogger("frameoverframe")
+
 
 Image.MAX_IMAGE_PIXELS = 244022272
 
