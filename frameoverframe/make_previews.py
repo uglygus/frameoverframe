06d8a019271573c5c2d15393ff6d26abab67de7d
uglygus <cooperbattersby@gmail.com>
2021-07-04 17:13:27 -0400

    refactoring

--- frameoverframe/make_previews.py
+++ frameoverframe/make_previews.py
@@ -30,14 +30,33 @@ from frameoverframe.utils import ext_list, sorted_listdir
 log = logging.getLogger("frameoverframe")
 
 
+def check_folder_for_RAW_extensions(src_dir):
+
+    contains_raw_ext = False
+    was_the_list_of_dirs_modified = False
+
+    extensions = ext_list(src_dir)
+    log.debug("Checking folder for RAW extensions.")
+    for ext, raw_ext in product(extensions, RAW_EXTENSIONS):
+        if re.search(rf"{ext}$", raw_ext, re.IGNORECASE):
+            if unmix(src_dir):
+                log.info(f"{src_dir} was unmixed I will need double check all this.")
+                was_the_list_of_dirs_modified = True
+            log.debug("RAW Extension found, skipping '%s'" % (raw_ext))
+            contains_raw_ext = True
+    print("check_for... returning (%s, %s)", (contains_raw_ext, was_the_list_of_dirs_modified))
+
+    input(";;;")
+    return (contains_raw_ext, was_the_list_of_dirs_modified)
+
+
 def _make_previews(src_dir):
     init()  # colorama
-    was_the_list_of_dirs_modified = False
 
     log.debug("make_previews : in_dir = %s" % (src_dir))
 
     for item in sorted_listdir(src_dir):
-        skip_this_dir = False
+        contains_raw_ext = False
 
         if os.path.isfile(item):
             log.debug(f"'{item}' -- {Fore.RED}SKIPPING{Style.RESET_ALL}not a directory.")
@@ -47,27 +66,24 @@ def _make_previews(src_dir):
             log.info(f"Video file for '{item}' already exists.")
             continue
 
-        log.info(f"There is no video for: '{item}'")
-        extensions = ext_list(item)
+        #        log.info(f"There is no video for: '{item}'")
 
+        extensions = ext_list(item)
         if extensions == [""]:
             log.info(
                 f"'{item}' -- {Fore.RED}SKIPPING{Style.RESET_ALL} directory contains only directories."
             )
             continue
 
-        log.debug("Checking folder for RAW extensions.")
-        for ext, raw_ext in product(extensions, RAW_EXTENSIONS):
-            if re.search(rf"{ext}$", raw_ext, re.IGNORECASE):
-                log.debug("RAW Extension found, %s needs to unmix." % (raw_ext))
-                if unmix(item):
-                    log.info(f"{item} was unmixed I will need double check all this.")
-                    was_the_list_of_dirs_modified = True
-                skip_this_dir = True
-                break
+        (contains_raw_ext, was_this_list_of_dirs_modified) = check_folder_for_RAW_extensions(item)
+        if was_this_list_of_dirs_modified:
+            was_the_list_of_dirs_modified = True
 
-        if skip_this_dir:
-            log.debug(f"'{item}' got unmixed so do not make a video.")
+        if contains_raw_ext:
+            log.debug("Why and how did we get here?")
+            log.debug(
+                f"'{item}' contains RAW images but was not correctly unmixed. Do not make a video."
+            )
             continue
 
         log.info(f"Making video for '{item}'")
