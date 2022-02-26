d7592373b8be1f4c3a95ddeaf1da5d850d8f519c
uglygus <cooperbattersby@gmail.com>
2022-02-26 08:27:20 -0500

    30fps fix

--- frameoverframe/img2vid.py
+++ frameoverframe/img2vid.py
@@ -32,7 +32,7 @@ import tempfile
 from colorama import Fore, Style, init
 from PIL import Image
 
-from frameoverframe.config import RAW_EXTENSIONS
+from frameoverframe.config import RAW_EXTENSIONS, UNLINKABLE_FILES
 from frameoverframe.utils import me, sorted_listdir, test_one_extension
 
 log = logging.getLogger("frameoverframe")
@@ -95,7 +95,7 @@ def img2vid(input_dirs, output_file=None, profile="preview", framenumber=False):
         ffmpeg_settings = [
             "-loglevel", "error", '-stats',
         #    "-r", "24000/1001",
-            "-r", 30,
+            "-r", "30",
             "-vcodec", "libx264",
             "-pix_fmt", "yuv420p",
             "-preset", "veryfast",
@@ -115,7 +115,7 @@ def img2vid(input_dirs, output_file=None, profile="preview", framenumber=False):
         # fmt: off
         ffmpeg_settings = [
             #"-r", "24000/1001",
-            "-r", 30,
+            "-r", "30",
             "-vcodec", "libx264",
             "-crf", "17",
             "-pix_fmt", "yuv422p",
@@ -136,7 +136,7 @@ def img2vid(input_dirs, output_file=None, profile="preview", framenumber=False):
         # fmt: off
         ffmpeg_settings = [
             #"-r", "24000/1001",
-            "-r", 30,
+            "-r", "30",
             "-vcodec", "dnxhd",
             "-profile:v", "dnxhr_sq",
             "-pix_fmt", "yuv422p",
@@ -192,7 +192,8 @@ def img2vid(input_dirs, output_file=None, profile="preview", framenumber=False):
         this_dir_images = sorted_listdir(input_dir)
 
         for image in this_dir_images:
-            if image.endswith(".DS_Store"):
+            if image in UNLINKABLE_FILES:
+                #    if image.endswith(".DS_Store"):
                 continue
             if image.endswith(".CR2"):
                 print("ERROR: CR2 is not a recognized image format for ffmpeg.", image)
